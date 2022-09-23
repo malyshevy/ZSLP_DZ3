ef8198466d1ff5df6a07e632af7e0558a139429c
Yuri Malyshev <team21_07@list.ru>
2022-09-23 21:33:42 +0300

    task023 complete

---
+++ task023/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 23 : Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.Write("Введите число : ");
+double num=double.Parse(Console.ReadLine());
+double num2;
+for (int i=1;i<=num;i++)
+{
+    Console.Write($"{Math.Pow(i,3)} ; ");
+}
