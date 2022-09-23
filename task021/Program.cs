bf4ec6e1948cbd45318cde2a92f67be84533358d
Yuri Malyshev <team21_07@list.ru>
2022-09-23 21:10:24 +0300

    task021 complete

---
+++ task021/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 21 : Напишите программу, которая принимает на вход координаты двух точек 
+//и находит расстояние между ними в 3D пространстве.
+Console.Clear();
+Console.WriteLine("Введите координаты первой точки");
+Console.Write("x : ");
+double x=double.Parse(Console.ReadLine());
+Console.Write("y : ");
+double y=double.Parse(Console.ReadLine());
+Console.Write("z : ");
+double z=double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки");
+Console.Write("x : ");
+double x1=double.Parse(Console.ReadLine());
+Console.Write("y : ");
+double y1=double.Parse(Console.ReadLine());
+Console.Write("z : ");
+double z1=double.Parse(Console.ReadLine());
+double rezult=Math.Abs(Math.Sqrt(Math.Pow((x1-x),2)+Math.Pow((y1-y),2)+Math.Pow((z1-z),2)));
+Console.WriteLine($"Расстояние между точками равно : {Math.Round(rezult,2)}");
